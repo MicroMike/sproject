2d142c351462ae14731f6e9e9b42a881f8f72be3
Mike <assoune.mike@gmail.com>
2022-12-31 18:27:04 +0100

    c

---
+++ index.js
@@ -0,0 +1,15 @@
+import shell from 'shelljs'
+
+const l = shell.exec('pidof node', { silent: true })
+const nodePids = l.stdout.split(' ').map(p => String(Number(p)))
+
+shell.exec('npm run s', { silent: true })
+
+setInterval(() => {
+	const list = shell.exec('pidof node', { silent: true })
+	const pids = list.stdout.split(' ').map(p => String(Number(p))).filter((p) => !nodePids.includes(p))
+
+	shell.exec(`kill -9 ${pids.join(' ')}`, { silent: true })
+
+	shell.exec('npm run s', { silent: true })
+}, 1000 * 60 * 60 * 3)
