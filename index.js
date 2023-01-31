6a7a78749b9b9e1571696a192968d09b685bb3ea
Mike <assoune.mike@gmail.com>
2023-01-31 17:50:20 +0100

    c

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const nodePids = l.stdout.split(' ').map(p => String(Number(p)))
 
 shell.exec('npm run s')
 
-setInterval(() => {
-	const list = shell.exec('pidof node', { silent: true })
-	const pids = list.stdout.split(' ').map(p => String(Number(p))).filter((p) => !nodePids.includes(p))
+// setInterval(() => {
+// 	const list = shell.exec('pidof node', { silent: true })
+// 	const pids = list.stdout.split(' ').map(p => String(Number(p))).filter((p) => !nodePids.includes(p))
 
-	shell.exec(`kill -9 ${pids.join(' ')}`, { silent: true })
+// 	shell.exec(`kill -9 ${pids.join(' ')}`, { silent: true })
 
-	shell.exec('npm run s')
-}, 1000 * 60 * 60 * 24)
+// 	shell.exec('npm run s')
+// }, 1000 * 60 * 60 * 24)
