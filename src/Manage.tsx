1aa597ceee459346f849e57e31d89f15e54f0fa7
Mike <assoune.mike@gmail.com>
2023-06-26 23:14:09 +0200

    c

--- src/Manage.tsx
+++ src/Manage.tsx
@@ -50,7 +50,7 @@ const update = (account: string, key: string, value: string, cb: () => void) => 
 const Manage = () => {
 	const [accounts, setAccounts] = useState<any>([])
 	const [filterKey, setFilterKey] = useState<{ k: string, asc: boolean }>({ k: '', asc: true })
-	const [searchValue, setSearchValue] = useState<{ [K: string]: any }>([])
+	const [searchValue, setSearchValue] = useState<{ [K: string]: any }>({ del: 'false' })
 	const [socket, setSocket] = useState<any>()
 	const [copy, setCopy] = useState(false)
 
